a67543bc268b858478191a7b2c63c0e4956694a2
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-09 21:40:51 +0530

    Create stop.py

---
+++ Deadly/plugins/stop.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Deadly import HNDLR, Music
+from Deadly.helpers.decorators import authorized_users_only
+from Deadly.helpers.queues import QUEUE, clear_queue
+
+@Client.on_message(filters.command(["end", "stop"], prefixes=f"{HNDLR}"))
+@authorized_users_only
+async def stop(client, m: Message):
+    await m.delete()
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await Music.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await m.reply("**✅ Streaming Stopped**")
+        except Exception as e:
+            await m.reply(f"**ERROR** \n`{e}`")
+    else:
+        await m.reply("**❌ Nothing is playing to end!**")

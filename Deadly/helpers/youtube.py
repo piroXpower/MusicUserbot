042caa74dbe7d1c0d4f2a3608a952c5ff0e3f078
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-12 18:02:09 +0530

    Create youtube.py

---
+++ Deadly/helpers/youtube.py
@@ -0,0 +1,57 @@
+# Copyright © 2023-2024 by piroxpower@Github, < https://github.com/piroxpower >.
+#
+# This file is part of < https://github.com/Team-Deadly/MusicUserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Team-Deadly/MusicUserbot/blob/main/LICENSE >
+#
+# All rights reserved ®.
+
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pytgcalls import StreamType
+from youtubesearchpython import VideosSearch
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+# music player
+def ytsearch(query):
+    try:
+        search = VideosSearch(query, limit=1)
+        for r in search.result()["result"]:
+            ytid = r["id"]
+            if len(r["title"]) > 34:
+                songname = r["title"][:35] + "..."
+            else:
+                songname = r["title"]
+            url = f"https://www.youtube.com/watch?v={ytid}"
+            duration = r["duration"]
+        return [songname, url, duration]
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def ytdl(link):
+    proc = await asyncio.create_subprocess_exec(
+        "yt-dlp",
+        "-g",
+        "-f",
+        # CHANGE THIS BASED ON WHAT YOU WANT
+        "bestaudio",
+        f"{link}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        return 1, stdout.decode().split("\n")[0]
+    else:
+        return 0, stderr.decode()
+

5c321c8f756e733dfd3ed56a2358857dc7435d90
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-27 12:41:59 +0530

    Update resume.py

--- Deadly/plugins/resume.py
+++ Deadly/plugins/resume.py
@@ -1,11 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from Deadly import HNDLR, Music
+from Deadly import HNDLR, Music, SUDOERS
 from Deadly.helpers.decorators import authorized_users_only
 from Deadly.helpers.queues import QUEUE
 
-@Client.on_message(filters.command(["resume"], prefixes=f"{HNDLR}"))
-@authorized_users_only
+@Client.on_message(filters.user(SUDOERS) & filters.command(["resume"], prefixes=f"{HNDLR}"))
 async def resume(client, m: Message):
     await m.delete()
     chat_id = m.chat.id

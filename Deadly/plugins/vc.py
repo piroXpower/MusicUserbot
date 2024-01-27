63333506d7b87493a1cfb542fde8f5ea5d72c7a4
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-27 12:40:17 +0530

    Update vc.py

--- Deadly/plugins/vc.py
+++ Deadly/plugins/vc.py
@@ -1,37 +1,23 @@
 from random import randint
 from pyrogram import Client, filters
-from Deadly import HNDLR, PLAYER as USER
+from Deadly import HNDLR, PLAYER as USER, SUDOERS
 from Deadly.helpers.decorators import authorized_users_only
 from pyrogram.errors import UserAlreadyParticipant
 from pyrogram.raw.functions.phone import CreateGroupCall
 
 
-@Client.on_message(filters.command(["join"], prefixes=f"{HNDLR}"))
-@authorized_users_only
+@Client.on_message(filters.user(SUDOERS) & filters.command(["join"], prefixes=f"{HNDLR}"))
 async def join(client, message):
-    chat_id = message.chat.id
+    flags = " ".join(message.command[1:])
     try:
-        link = await client.export_chat_invite_link(chat_id)
-    except BaseException:
-        await message.reply("**Error:**\nAdd me as admin first!")
-        return
-    try:
-        await USER.join_chat(link)
+        await USER.join_chat(flags)
         await message.reply("**Userbot Joined**")
     except UserAlreadyParticipant:
         await message.reply("**Userbot already participants of the given link**")
 
 
-@Client.on_message(filters.command(["openvc"], prefixes=f"{HNDLR}"))
-@authorized_users_only
-async def opengc(client, message):
-    flags = " ".join(message.command[1:])
-    if flags == "channel":
-        chat_id = message.chat.title
-        type = "channel"
-    else:
-        chat_id = message.chat.id
-        type = "group"
+@Client.on_message(filters.user(SUDOERS) & filters.command(["openvc"], prefixes=f"{HNDLR}"))
+async def opengc(client, message):    
     try:
         await USER.send(CreateGroupCall(
             peer=(await USER.resolve_peer(chat_id)),

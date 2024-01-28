0c09102597b5ff147a4d3525e6fe81cd9f8157d1
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-28 07:16:34 +0530

    Update vc.py

--- Deadly/plugins/vc.py
+++ Deadly/plugins/vc.py
@@ -1,9 +1,6 @@
-from random import randint
 from pyrogram import Client, filters
 from Deadly import HNDLR, PLAYER as USER, SUDOERS
-from Deadly.helpers.decorators import authorized_users_only
 from pyrogram.errors import UserAlreadyParticipant
-from pyrogram.raw.functions.phone import CreateGroupCall
 
 
 @Client.on_message(filters.user(SUDOERS) & filters.command(["join"], prefixes=f"{HNDLR}"))
@@ -16,15 +13,3 @@ async def join(client, message):
         await message.reply("**Userbot already participants of the given link**")
 
 
-@Client.on_message(filters.user(SUDOERS) & filters.command(["openvc"], prefixes=f"{HNDLR}"))
-async def opengc(client, message):    
-    try:
-        await USER.send(CreateGroupCall(
-            peer=(await USER.resolve_peer(chat_id)),
-            random_id=randint(10000, 999999999)
-        )
-        )
-    except Exception:
-        await message.reply(
-            "**Error:** Add userbot as admin of your group/channel with permission **Can manage voice chat**"
-        )

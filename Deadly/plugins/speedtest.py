8b4541bc5042782862b172d22a8ec006112726dd
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-28 07:50:07 +0530

    Update speedtest.py

--- Deadly/plugins/speedtest.py
+++ Deadly/plugins/speedtest.py
@@ -1,1 +1,56 @@
+from Deadly import PLAYER, OWNER_ID
+from pyrogram import filters, Client
+import speedtest
+import asyncio
 
+
+
+
+
+def testspeed(m):
+    try:
+        test = speedtest.Speedtest()
+        test.get_best_server()
+        m = m.edit("Running Download SpeedTest")
+        test.download()
+        m = m.edit("Running Upload SpeedTest")
+        test.upload()
+        test.results.share()
+        result = test.results.dict()
+        m = m.edit("Sharing SpeedTest Results")
+    except Exception as e:
+        return m.edit(e)
+    return result
+
+# 		Send Speed of Internet
+
+
+@Client.on_message((filters.command("/speedcheck") & filters.user(OWNER_ID))
+async def speedtest_function(client, message):
+    m = await message.reply_text("Running Speed test")
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, testspeed, m)
+    output = f"""**Speedtest Results**
+
+<u>**Client:**</u>
+**__ISP:__** {result['client']['isp']}
+**__Country:__** {result['client']['country']}
+**__ISP Rating:__** {result['client']['isprating']}
+
+<u>**Server:**</u>
+**__Name:__** {result['server']['name']}
+**__Country:__** {result['server']['country']}, {result['server']['cc']}
+**__Sponsor:__** {result['server']['sponsor']}
+**__Latency:__** {result['server']['latency']}
+**__Ping:__** {result['ping']}
+
+<u>**Speed:**</u>
+**__Download Speed:__** {result['download'] / 1024 / 1024:.2f} Mbps
+**__Upload Speed:__** {result['upload'] / 1024 / 1024:.2f} Mbps
+"""
+    msg = await app.send_photo(
+        chat_id=message.chat.id,
+        photo=result["share"],
+        caption=output
+    )
+    await m.delete()

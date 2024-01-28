d73a52af24124d378f280010b25e7ff3ee191038
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-28 12:14:37 +0530

    Update __main__.py

--- Deadly/__main__.py
+++ Deadly/__main__.py
@@ -1,12 +1,13 @@
 import asyncio
 from pytgcalls import idle
-from Deadly import PLAYER, Music
+from Deadly import PLAYER, Music, PM_GUARD
 from Deadly.helpers.join import join
 
 
 async def main():
     print("[INFO] Starting client...")
     await PLAYER.start()
+    await PM_GUARD.start() 
     await join(PLAYER) 
     print("[INFO] Client Started Successfully!") 
     print("[INFO] STARTING Musicplayer..")
@@ -23,7 +24,7 @@ async def main():
     await idle()
     print("[INFO] Userbot Stopped! Good Bye..")
     await PLAYER.stop()
-
+    await PM_GUARD.stop() 
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())

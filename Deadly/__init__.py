c074ff3a9d42bf915ffa6d2975c4f24508439504
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-03-03 17:42:49 +0530

    Update __init__.py

--- Deadly/__init__.py
+++ Deadly/__init__.py
@@ -8,7 +8,7 @@ print("[INFO] Deadly Music Userbot Setup Starting... ")
 
 API_ID = int(os.getenv("API_ID", "21364355")) 
 API_HASH = os.getenv("API_HASH", " 72f11aec1dd3e5764554d477341a3d0b") 
-PYRO_STRING = os.getenv("STRING_SESSION", None) 
+PYRO_STRING = os.getenv("STRING_SESSION", "BQGMcpgANEogDdWKKevmhk_nLwZwJgNo-wX53dApL2rG_7NVsWr1PMRAQ5IYw52b2UtOsDER0tn6pMM0QCmgj6bbvzbYZf6eLSXrKEBBuCtf76XsXaWrjkWp693GX3nLVzY7dEpgE8c7UqHv1HfOFwEjOM7gOAcUlwwGCIwIbn8NhFBHjUtAZfWSwz-UxLrNU2FCaehCF2duPM0h33L53oPX3XMqy90DRs-vMcHgR9U_7l6qE0j_o9oEJBlsaC50xoQFQNqgiFIXL4BbFtShfUK6OfXikxNm88_bqVeZfdTBbdAiY7rKJON2ZuE2VhBmRO8VD35EwaFsJfgLcXRvoph9tFV6mQAAAAFQgoOSAA") 
 STRING2 = os.getenv("STR2", "BQFF_oMAkTEJiWjGBHcny80muJMoBqdiqwcRoX55n9rslGQTayIv4xOcrHoyhHM5JUQnjbTl-An1RnhLeVIcFHL3keHVFRRQe7Xwo4aqaz5pkenGijhZCcb9LXENaqUjo8KLvncHbdpD2KT4aFl_9TsWUMFhfttw2boauJIISUmozA3pWwxm4He0oSLyFMeayY_jzlfXMsvdQXoXPWxDpZ0nrlgleVXI1JfFk3NI6vzP7bI9EQpudGAftRQAZev-rgyZeHEgnPMAUsLOceM_CdzrzgdcFfnpPLa4Y8EYeXLrO_VfUv-W-_sx2vu8pauaQbnMBeOcae6kWWNtUHKjgg4jeuNvnwAAAAGH2ProAA") 
 OWNER_ID = os.getenv("OWNER_ID", "6574111464") 
 MONGO_URI = os.getenv("MONGO_URL", "mongodb+srv://jarvis:op@cluster0.7tisvwv.mongodb.net/?retryWrites=true&w=majority") 

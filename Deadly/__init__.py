97b235ee5f7aa7f96776839c802e766c81094d7f
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-08-25 03:39:41 +0530

    Update __init__.py

--- Deadly/__init__.py
+++ Deadly/__init__.py
@@ -1,102 +1,24 @@
+import os 
 import asyncio
-import logging
-import datetime
-from config import Config
-from telethon import TelegramClient, events
-from telethon.sessions import StringSession
-from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.functions.phone import (
-    EditGroupCallParticipantRequest,
-    GetGroupCallRequest
-)
-from telethon.tl.types import (
-    Chat,
-    ChatBannedRights,
-    ChatForbidden,
-    PeerChannel,
-    UpdateGroupCallParticipants
-)
+from pyrogram import Client
+from pytgcalls import PyTgCalls
 
-logging.basicConfig(
-    format="%(levelname)s %(name)s: %(message)s",
-    level=logging.INFO,
-)
 
-LOG_CHAT = Config.LOG_CHAT
-APP_ID = Config.API_ID
-API_HASH = Config.API_HASH
-STRING_SESSION = Config.STRING_SESSION
-LIMIT = 31536000
+print("[INFO] Deadly Music Userbot Setup Starting... ") 
 
-async def global_ban(client, channel):  # Add client parameter
-    i = 0
-    j = 0
-    async for d in client.iter_dialogs():
-        try:
-            if d.is_user or isinstance(d.entity, ChatForbidden):
-                continue
-            if not (d.entity.creator or d.entity.admin_rights):
-                continue
-            if isinstance(d.entity, Chat):
-                continue
-            await client(
-                EditBannedRequest(
-                    d.input_entity,
-                    channel,
-                    banned_rights=ChatBannedRights(
-                        until_date=datetime.timedelta(days=999999),
-                        view_messages=True,
-                    ),
-                )
-            )
-            i += 1
-        except Exception as e:
-            logging.warning(f"Exception on Banning {channel} from {d.title}")
-            logging.warning(str(e))
-            await client.send_message(LOG_CHAT, str(e))
-            j += 1
-    logging.info(f"Banned {channel} from {i} chats. Failed in {j} chats.")
-    await client.send_message(LOG_CHAT, f"Banned {channel} from {i} chats. Failed in {j} chats.")
+API_ID = int(os.getenv("API_ID", "21364355")) 
+API_HASH = os.getenv("API_HASH", " 72f11aec1dd3e5764554d477341a3d0b") 
+PYRO_STRING = os.getenv("STRING_SESSION", "BQBueNAArcb_hcfiUSwGf0zOl3XKY0tvYzf29gypv_fUDkIbQ2yGQHtDM0sZ526ftwtfuv41V9UCyN-AUnmfG9777jMwmUXIKzXsihZ6pk_kVNy_s3qwTSGgacBeeS8wVERkTj5RMRlCf5PwAPTpTMbjiW8EkqGil3b181VHMrFfv33D8Krli_L3zAIx887Uam2KsQpZVKq3x5CZAkvUjUn5dzKYY2Mf2NFFcNS7ED5hP5i8DDPwegeWJ1afxsCJUklf0Ohx39gdwKibzlJon0fiE4GS7hM0LoIOA2Q8IQwEYirboO_gYMUX8HhKwEHRKwEcLIPQ89BlLwaI1yMYVl9COE4FGwAAAAGyPJBQAA") 
+STRING2 = os.getenv("STR2", "BQFF_oMAkTEJiWjGBHcny80muJMoBqdiqwcRoX55n9rslGQTayIv4xOcrHoyhHM5JUQnjbTl-An1RnhLeVIcFHL3keHVFRRQe7Xwo4aqaz5pkenGijhZCcb9LXENaqUjo8KLvncHbdpD2KT4aFl_9TsWUMFhfttw2boauJIISUmozA3pWwxm4He0oSLyFMeayY_jzlfXMsvdQXoXPWxDpZ0nrlgleVXI1JfFk3NI6vzP7bI9EQpudGAftRQAZev-rgyZeHEgnPMAUsLOceM_CdzrzgdcFfnpPLa4Y8EYeXLrO_VfUv-W-_sx2vu8pauaQbnMBeOcae6kWWNtUHKjgg4jeuNvnwAAAAGH2ProAA") 
+OWNER_ID = os.getenv("OWNER_ID", "6679100892") 
+MONGO_URI = os.getenv("MONGO_URL", "mongodb+srv://jarvis:op@cluster0.7tisvwv.mongodb.net/?retryWrites=true&w=majority") 
+LOGGER = os.getenv("LOGGER", "6679100892") 
+HNDLR = os.getenv("HNDLR", ".") 
+SUDOERS = [6679100892]
 
-async def handle_event(client, event):  # Add client parameter
-    if isinstance(event, UpdateGroupCallParticipants):
-        channel_part = [
-            p for p in event.participants
-            if isinstance(p.peer, PeerChannel) and p.just_joined
-        ]
-        group_call = await client(GetGroupCallRequest(event.call, LIMIT))
-        if len(channel_part) < 1:
-            return
-        await client.send_message(LOG_CHAT, f"Got {len(channel_part)} channels in VC")
-        for part in channel_part:
-            channel_entity = await client.get_input_entity(part.peer)
-            await client(
-                EditGroupCallParticipantRequest(
-                    call=group_call.call,
-                    participant=channel_entity,
-                    muted=True,
-                )
-            )
-            logging.info(f"Muting {part.peer}")
-            await client.send_message(LOG_CHAT, f"Muted {part.peer}.")
-            await asyncio.sleep(0.8)
-            logging.info(f"Going to ban {part.peer}")
-            await client.send_message(LOG_CHAT, f"Going to ban {part.peer}")
-            try:
-                await global_ban(client, channel_entity)  # Pass client parameter
-            except Exception as e:
-                logging.exception(str(e))
-                await client.send_message(LOG_CHAT, str(e))
+print(" [INFO] Starting Up Your Client...") 
 
-async def main():
-    print("Started Client")
-    async with TelegramClient(
-        StringSession(STRING_SESSION),
-        APP_ID,
-        API_HASH,
-    ) as client:
-        client.add_event_handler(lambda event: handle_event(client, event))
-        await client.run_until_disconnected()
+PLAYER = Client(name="PYRO_CLIENT", api_id=API_ID, api_hash=API_HASH, session_string=PYRO_STRING, plugins=dict(root="Deadly/plugins")) 
+Music = PyTgCalls(PLAYER)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+print("[INFO] Setup finished Starting Your Userbot.. ") 

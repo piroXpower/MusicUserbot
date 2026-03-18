4739c640136a457ce9fbcc1ed5ca75e41a07600a
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2026-03-19 00:04:34 +0530

    Update help.py

--- Deadly/plugins/help.py
+++ Deadly/plugins/help.py
@@ -1,40 +1,45 @@
-# Copyright © 2023-2024 by piroxpower@Github, < https://github.com/piroxpower >.
-#
-# This file is part of < https://github.com/Team-Deadly/MusicUserbot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Team-Deadly/MusicUserbot/blob/main/LICENSE >
-#
-# All rights reserved ®.
+# Copyright © 2023-2024 by piroxpower@Github
+# Optimized for 2026: Cyber-Aura Help Menu
 
-import os
-import sys
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Deadly import HNDLR
 
-
-@Client.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
-async def help(client, m: Message):
+@Client.on_message(filters.command(["help", "commands"], prefixes=f"{HNDLR}"))
+async def help_menu(client, m: Message):
+    # Keep the group clean by deleting the help trigger
     await m.delete()
-    HELP = f"""
-<b>👋 Hallo {m.from_user.mention}!
-🛠 MUSIC PLAYER HELP MENU
+    
+    # Styled Help Menu matching the Cyber-Aura theme
+    HELP_TEXT = f"""
+✨ **ᴅᴇᴀᴅʟʏ ᴍᴜsɪᴄ ᴜsᴇʀʙᴏᴛ** ✨
+━━━━━━━━━━━━━━━━━━━━━━
+👋 **ʜᴇʟʟᴏ {m.from_user.first_name}!**
+*ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴀᴜʀᴀ-sᴛʀᴇᴀᴍ ɢᴜɪᴅᴇ:*
 
-⚡ COMMAND FOR EVERYONE
-• {HNDLR}play [song title | youtube links | reply audio file] - to play the song
-• {HNDLR}playlist to view playlists
-• {HNDLR}ping - to check status
-• {HNDLR}help - to see a list of commands
-• {HNDLR}join- to join | to the group
-⚡ COMMAND FOR ALLOWED ADMINS
-• {HNDLR}resume - to continue playing a song or video
-• {HNDLR}pause - to pause playing a song or video
-• {HNDLR}skip - to skip a song or video
-• {HNDLR}end - to end playback
-⚡ COMMAND TO OWNER
-• {HNDLR}restart - to restart the deployment of bot
-• {HNDLR}eval - to check some code in terminal
-• {HNDLR}stats - to check user total data stats
-• {HNDLR}speedtest - to check speed of server
+🎧 **ɢᴇɴᴇʀᴀʟ ᴄᴏᴍᴍᴀɴᴅs**
+├ `{HNDLR}play` - sᴇᴀʀᴄʜ ᴊɪᴏsᴀᴀᴠɴ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀᴜᴅɪᴏ
+├ `{HNDLR}ping` - ᴄʜᴇᴄᴋ ʙᴏᴛ ʟᴀᴛᴇɴᴄʏ & sᴛᴀᴛᴜs
+├ `{HNDLR}join` - ᴍᴏᴠᴇ ᴜsᴇʀʙᴏᴛ ᴛᴏ ʏᴏᴜʀ ᴠᴄ
+└ `{HNDLR}help` - sʜᴏᴡ ᴛʜɪs ᴍᴇssᴀɢᴇ
+
+🛡️ **ᴀᴅᴍɪɴ ᴄᴏɴᴛʀᴏʟs**
+├ `{HNDLR}pause` - ʜᴀʟᴛ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ sᴛʀᴇᴀᴍ
+├ `{HNDLR}resume` - ᴄᴏɴᴛɪɴᴜᴇ ᴘᴀᴜsᴇᴅ ᴍᴜsɪᴄ
+├ `{HNDLR}skip` - ᴘʟᴀʏ ɴᴇxᴛ ᴛʀᴀᴄᴋ ɪɴ ǫᴜᴇᴜᴇ
+└ `{HNDLR}stop` - ᴇɴᴅ sᴇssɪᴏɴ & ᴡɪᴘᴇ ǫᴜᴇᴜᴇ
+
+👑 **ᴏᴡɴᴇʀ ᴇxᴄʟᴜsɪᴠᴇ**
+├ `{HNDLR}speedcheck` - ʀᴜɴ sᴇʀᴠᴇʀ ɴᴇᴛᴡᴏʀᴋ ᴀᴜᴅɪᴛ
+├ `{HNDLR}restart` - ʀᴇʙᴏᴏᴛ ᴛʜᴇ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ
+├ `{HNDLR}stats` - ᴠɪᴇᴡ ɢʟᴏʙᴀʟ ᴜsᴀɢᴇ ᴅᴀᴛᴀ
+└ `{HNDLR}eval` - ᴇxᴇᴄᴜᴛᴇ sʏsᴛᴇᴍ ᴄᴏᴅᴇ (ᴅᴀɴɢᴇʀ)
+━━━━━━━━━━━━━━━━━━━━━━
+✨ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇᴀᴅʟʏ ᴀᴜᴅɪᴛ ᴇɴɢɪɴᴇ**
 """
-    await m.reply(HELP)
+    
+    await m.reply_text(
+        text=HELP_TEXT,
+        disable_web_page_preview=True
+    )
+    

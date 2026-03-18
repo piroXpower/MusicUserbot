1233843f4ef6762497127eec43af99f63ef555ac
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2026-03-18 23:39:41 +0530

    Update join function to include new chat

--- Deadly/helpers/join.py
+++ Deadly/helpers/join.py
@@ -5,9 +5,6 @@ from Deadly import PLAYER
 
 async def join(PLAYER):
    try:        
-       await PLAYER.join_chat("TheDeadlyBots") 
-       await PLAYER.join_chat("TheBotUpdates") 
-       await PLAYER.join_chat("hindi_international_chatting") 
-       await PLAYER.join_chat("international_hindi_chatting") 
+       await PLAYER.join_chat("ThriveGuy") 
    except Exception as e:
        print(f"{e}")
